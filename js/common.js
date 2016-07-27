7163b8e2d81f4bc57e3bc6012b93aac5046e9375
Dmitriy Stepanchuk <d.stepanchuk@intend.kiev.ua>
2016-07-27 17:46:33 +0300

    add popup

--- js/common.js
+++ js/common.js
@@ -1,1 +1,18 @@
-console.log("man");
+window.onload = function() {
+
+    var modal = document.getElementById("modal"),
+        modalOverlay = document.getElementById("modal-overlay"),
+        closeButton = document.getElementById("close-button"),
+        openButton = document.getElementById("open-button");
+
+    closeButton.addEventListener("click", function() {
+        modal.classList.toggle("closed");
+        modalOverlay.classList.toggle("closed");
+    });
+
+    openButton.addEventListener("click", function() {
+        modal.classList.toggle("closed");
+        modalOverlay.classList.toggle("closed");
+    });
+
+};
